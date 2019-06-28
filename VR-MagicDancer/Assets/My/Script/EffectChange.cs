38df59f97963a46e94f3a761e1ef0038e466a439
d <d>
2019-06-28 22:02:43 +0900

    作成

---
+++ VR-MagicDancer/Assets/My/Script/EffectChange.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectChange : MonoBehaviour
+{
+    Renderer color;
+
+    public GameObject touchOn;
+    public GameObject touchOff;
+
+    public Material on;
+    public Material off;
+
+    GameObject onEffect;
+    GameObject offEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = GetComponent<Renderer>();
+
+        onEffect = Instantiate(touchOn, transform) as GameObject;
+        offEffect = Instantiate(touchOff, transform) as GameObject;
+
+        onEffect.SetActive(true);
+        onEffect.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        color.material = on;
+        onEffect.SetActive(false);
+        onEffect.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        color.material = off;
+        onEffect.SetActive(true);
+        onEffect.SetActive(false);
+    }
+}
---
+++ VR-MagicDancer/Assets/My/Script/Example.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class Example : MonoBehaviour
+{
+
+    Vector3 baseScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseScale = transform.localScale;
+    }
+    private void Update()
+    {
+
+        if (Music.IsPlaying && Music.IsJustChangedBeat())
+        {
+            gameObject.transform.DOScale(baseScale * 3f, 0.0f);
+            gameObject.transform.DOScale(baseScale, 0.2f);
+        }
+    }
+}
